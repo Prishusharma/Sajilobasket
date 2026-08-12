b72247bf066eb06200b16550699a25c100ca2a81
rajasbibogati <rajasbi.bogati18@gmail.com>
2026-08-12 12:25:35 +0545

    user migrations

--- sajilo_basket/settings.py
+++ sajilo_basket/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'basket',
     'rest_framework',
+    'user',
+    'products',
+    'orders',
 ]
+
+AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "phone",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "phone",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("SajiloBasket Information", {
+            "fields": ("role", "phone"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("SajiloBasket Information", {
+            "fields": ("role", "phone"),
+        }),
+    )
